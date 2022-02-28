146733f3e7f92cc2dacb61d9fbcc08ae09643360
swagath shetty <swagath.shetty@gmail.com>
2022-02-28 18:15:52 +0530

    added basic text for app

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,37 @@ class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title:Text('Ninja Id Card'),
         centerTitle:true,
-        backgroundColor:Colors.grey
+        backgroundColor:Colors.grey,
+        elevation:0
       ),
+      body:Padding(
+        padding:EdgeInsets.fromLTRB(30, 40, 30,0),
+        child:Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Name',
+              style:TextStyle(
+                color:Colors.grey,
+                letterSpacing: 2
+              )
+            ),
+            Text(
+                'John Doe',
+                style:TextStyle(
+                    color:Colors.amber[200],
+                    letterSpacing: 2,
+                    fontSize:28,
+                    fontWeight:FontWeight.bold
+                )
+            ),
+          ],
+        )
+      )
     );
   }
 }
